be71c809905c6378a9dc9fd8bbb3072b2745bd15
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-09-19 19:58:25 -0300

    Webpack, package, tsconfig.json, gitignore

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const resolve = require('path').resolve;
+
+const app_name = 'minha-lista-de-compras'
+const path_to_static = resolve(__dirname, app_name, 'static')
+
+module.exports = {
+    entry: `./${app_name}/ts_compile_js/list.js`,
+    output: {
+        filename: 'list_index.js',
+        path: resolve(path_to_static, 'js')
+    }
+};
