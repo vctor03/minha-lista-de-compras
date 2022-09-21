6d6f1d95bbc050e31a397f358e6328fa471abfeb
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-09-21 17:02:16 -0300

    Update user route

--- minha-lista-de-compras/routes/user_route/user_route.py
+++ minha-lista-de-compras/routes/user_route/user_route.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-user = Blueprint('user', __name__, template_folder='/templates')
+user = Blueprint('user', __name__, template_folder='templates')
 
 @user.route('/')
 def index():
     return 'User Route'
+
+@user.route('/list')
+def list_route():
+    return render_template('list/index.html')
